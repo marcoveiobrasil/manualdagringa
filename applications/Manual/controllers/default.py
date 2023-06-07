dbfceb34b2d0b3fbf66c8e314d7e810885ecf8d7
marcoveiobrasil <marcoveioarts@gmail.com>
2023-06-07 16:21:00 +0200

    index page and blog page styling

--- applications/Manual/controllers/default.py
+++ applications/Manual/controllers/default.py
@@ -6,30 +6,41 @@
 
 # ---- example index page ----
 def index():
-    response.flash = T("Hello World")
-    return dict(message=T('Welcome to web2py!'))
+    return dict()
 
 # ---- API (example) -----
+
+
 @auth.requires_login()
 def api_get_user_email():
-    if not request.env.request_method == 'GET': raise HTTP(403)
-    return response.json({'status':'success', 'email':auth.user.email})
+    if not request.env.request_method == 'GET':
+        raise HTTP(403)
+    return response.json({'status': 'success', 'email': auth.user.email})
 
 # ---- Smart Grid (example) -----
-@auth.requires_membership('admin') # can only be accessed by members of admin groupd
+
+
+# can only be accessed by members of admin groupd
+@auth.requires_membership('admin')
 def grid():
-    response.view = 'generic.html' # use a generic view
+    response.view = 'generic.html'  # use a generic view
     tablename = request.args(0)
-    if not tablename in db.tables: raise HTTP(403)
-    grid = SQLFORM.smartgrid(db[tablename], args=[tablename], deletable=False, editable=False)
+    if not tablename in db.tables:
+        raise HTTP(403)
+    grid = SQLFORM.smartgrid(db[tablename], args=[
+                             tablename], deletable=False, editable=False)
     return dict(grid=grid)
 
 # ---- Embedded wiki (example) ----
+
+
 def wiki():
-    auth.wikimenu() # add the wiki to the menu
-    return auth.wiki() 
+    auth.wikimenu()  # add the wiki to the menu
+    return auth.wiki()
 
 # ---- Action for login/register/etc (required for auth) -----
+
+
 def user():
     """
     exposes:
@@ -49,6 +60,8 @@ def user():
     return dict(form=auth())
 
 # ---- action to server uploaded static content (required) ---
+
+
 @cache.action()
 def download():
     """
@@ -56,3 +69,7 @@ def download():
     http://..../[app]/default/download/[filename]
     """
     return response.download(request, db)
+
+
+def blog():
+    return dict()
